d87f7508edc94889db54b0525493cae619f3996e
Biyori <Grzany10@gmail.com>
2025-03-19 04:33:25 +0100

    fixed movement system

--- core/entity.py
+++ core/entity.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from core.grid import Grid
 
 class Entity:
@@ -13,8 +14,9 @@ class Entity:
 
         self.dragging = False
         self.startX, self.startY = self.gridX, self.gridY
+        self.tempPath = []
 
-    def initialize(self, x: int, y: int, grid: Grid):
+    def initialize(self, x: int, y: int, grid: Grid) -> None:
         self.gridX, self.gridY = x, y
         self.cellSize = grid.cellSize
         self.grid = grid
@@ -23,7 +25,7 @@ class Entity:
             y * self.cellSize + self.cellSize / 4
         )
 
-    def setPosition(self, x: int, y: int):
+    def setPosition(self, x: int, y: int) -> None:
         self.gridX, self.gridY = x, y
         self.position = pygame.Vector2(
             x * self.cellSize + self.cellSize / 4, 
@@ -33,28 +35,38 @@ class Entity:
     def draw(self, surface: pygame.Surface):
         pygame.draw.rect(surface, self.color, (*self.position, self.size, self.size))
 
-    def handleEvent(self, event):
+    def handleEvent(self, event: pygame.event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouseX, mouseY = event.pos
-            if self.isHovering(mouseX, mouseY):
+            if event.button == 1 and self.isHovering(mouseX, mouseY):
                 self.dragging = True
                 self.startX, self.startY = self.gridX, self.gridY
+                self.grid.path = [(self.startX, self.startY)]
 
-        elif event.type == pygame.MOUSEMOTION:
-            if self.dragging:
-                self.position = pygame.Vector2(event.pos) - pygame.Vector2(self.size/2, self.size/2)
+            elif event.button == 3 and self.dragging:
+                newX = math.floor(event.pos[0] / self.cellSize)
+                newY = math.floor(event.pos[1] / self.cellSize)
+                if len(self.grid.path) > 1:
+                    self.tempPath = (self.grid.path[:-1])
+                    self.startX, self.startY = self.grid.path[-1]         
 
-                newX = round(self.position.x / self.cellSize)
-                newY = round(self.position.y / self.cellSize)
+        elif event.type == pygame.MOUSEMOTION and self.dragging:
+            self.position = pygame.Vector2(event.pos) - pygame.Vector2(self.size/2, self.size/2)
+            newX = math.floor(event.pos[0] / self.cellSize)
+            newY = math.floor(event.pos[1] / self.cellSize)
 
-                self.grid.updatePath((self.startX, self.startY), (newX, newY), self.maxTiles * 5)
+            tempPath = self.grid.calculatePath((self.startX, self.startY), (newX, newY))
+
+            if len(tempPath) + len(self.tempPath) <= self.maxTiles + 1:
+                self.grid.path = self.tempPath + tempPath
                 print(self.grid.path)
         
-        elif event.type == pygame.MOUSEBUTTONUP:
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
             if self.dragging:
                 self.dragging = False
                 self.snapToGrid()
 
+                self.tempPath = []
                 self.grid.path = []
 
     def isHovering(self, mouseX, mouseY):
@@ -62,15 +74,20 @@ class Entity:
                 self.position.y <= mouseY <=self.position.y + self.size)
     
     def snapToGrid(self):
-        newX = round(self.position.x / self.cellSize)
-        newY = round(self.position.y / self.cellSize)
-
-        dx = abs(newX - self.startX)
-        dy = abs(newY - self.startY)
-
-        distance = max(dx, dy)
-
-        if distance <= self.maxTiles:
-            self.setPosition(newX, newY)
+        if self.isOutOfPath():
+            self.setPosition(self.grid.path[0][0], self.grid.path[0][1])
         else:
-            self.setPosition(self.startX, self.startY)
+            lastX, lastY = self.grid.path[-1]
+            self.setPosition(lastX, lastY)
+
+    def isOutOfPath(self):
+        if not self.grid.path:
+            return False
+        
+        currentX = math.floor(self.position.x / self.cellSize)
+        currentY = math.floor(self.position.y / self.cellSize)
+        startX, startY = self.grid.path[0]
+
+        distance = max(abs(currentX - startX), abs(currentY - startY))
+
+        return distance > self.maxTiles
--- core/grid.py
+++ core/grid.py
@@ -7,15 +7,6 @@ class Grid:
         self.matrix = [[0 for _ in range(10)] for _ in range(10)]
         self.path = []
 
-    def updatePath(self, startPos, currentPos, maxMovement):
-        path = self.calculatePath(startPos, currentPos)
-        cost = self.calculateMovementCost(path)
-
-        if cost <= maxMovement:
-            self.path = path
-        else:
-            self.path = []
-
     def calculatePath(self, startPos: pygame.Vector2, endPos: pygame.Vector2) -> list[pygame.Vector2]:
         startX, startY = startPos
         endX, endY = endPos
@@ -43,10 +34,7 @@ class Grid:
 
         path.append((endX, endY))   
         return path
-
-    def calculateMovementCost(self, path):
-        return len(path[1:]) * 5
-
+    
     def draw(self, screen: pygame.Surface) -> None:
         for y, row in enumerate(self.matrix):
             for x, _ in enumerate(row):
